e1386fe78437bb4608b7bbfbcd55bd6c14658d8d
Stephan Kulla <git.mail@kulla.me>
2021-02-15 21:30:15 +0100

    Paths have maximum depth of 5

--- shorten_paths.py
+++ shorten_paths.py
@@ -1,9 +1,10 @@
 import fileinput
 
-blacklist = ["packages", "public", "private", "module", "src"]
+BLACKLIST = ["packages", "public", "private", "module", "plugins"]
+MAX_PATH_DEPTH = 5
 
 for line in fileinput.input():
     entries = line.strip().split("|")
-    paths = [ p for p in entries[-1].split("/") if p not in blacklist ]
+    paths = [ p for p in entries[-1].split("/") if p not in BLACKLIST ]
 
-    print("|".join(entries[:-1]) + "|" + "/".join(paths))
+    print("|".join(entries[:-1]) + "|" + "/".join(paths[:MAX_PATH_DEPTH+1]))
